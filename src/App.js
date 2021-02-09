bbbc03617dfd0c65f4bc608f43e7ad245566824c
Cyriacus Ifeanyi <cyriacusifeanyi@gmail.com>
2021-02-09 20:08:56 +0100

    Completed: Exercises 6.13.-6.14.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Notification from './components/Notification'
 import Filter from './components/Filter'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
+import { useDispatch } from 'react-redux'
+import anecdoteService from './services/anecdotes'
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    anecdoteService
+      .getAll().then(anecdotes => dispatch(initializeAnecdotes(anecdotes)))
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   return (
     <div>
